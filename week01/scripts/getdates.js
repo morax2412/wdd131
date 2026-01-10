560f49080845cb357ef3886018351182613760c1
Cristian Inafuku <cloud2002.12.24@gmail.com>
2026-01-11 05:53:46 +0900

    page

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById("lastModified").textContent =
+    "Last Modification: " + document.lastModified;
