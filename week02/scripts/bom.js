da1716d5360d64f2f6d029e645fde1ec7f33fb2a
Cristian Inafuku <cloud2002.12.24@gmail.com>
2026-01-14 19:28:31 +0900

    W02 BOM DOM events

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', () => {
+  if (input.value.trim() === '') return;
+
+  const li = document.createElement('li');
+  const deleteButton = document.createElement('button');
+
+  li.textContent = input.value;
+
+  deleteButton.textContent = '❌';
+  deleteButton.ariaLabel = `Remove ${input.value}`;
+
+  li.append(deleteButton);
+  list.append(li);
+
+  input.value = '';
+  input.focus();
+
+  
+  deleteButton.addEventListener('click', () => {
+    list.removeChild(li);
+    input.focus();
+  });
+});
